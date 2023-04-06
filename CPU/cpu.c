27e7de9ab3a9be8ffc7e378e1f61ad591cb02cdd
LateForever <albertgrzegrzolka124@gmail.com>
2023-04-06 19:39:35 +0200

    define registers

--- CPU/cpu.c
+++ CPU/cpu.c
@@ -10,3 +10,14 @@
 #define MOV    0x03
 #define LOAD   0x04
 #define STORE  0x05
+
+// Define Registers
+typedef struct {
+	uint8_t a;
+	uint8_t b;
+	uint8_t c;
+	uint8_t d;
+	uint16_t ip; // Instructions pointer
+} CPU;
+
+
