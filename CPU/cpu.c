f379d03611e8e7391e474b3fbf1520ed54afbfe9
LateForever <albertgrzegrzolka124@gmail.com>
2023-04-06 19:37:39 +0200

    defined instruction set

--- CPU/cpu.c
+++ CPU/cpu.c
@@ -1,1 +1,12 @@
-//hello :)
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+// Define instructions set
+#define MEMORY_SIZE 256
+#define NUM_INSTRUCTIONS 6
+#define ADD    0x01
+#define SUB    0x02
+#define MOV    0x03
+#define LOAD   0x04
+#define STORE  0x05
